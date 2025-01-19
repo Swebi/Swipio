ca33a06d01e119a6e791d5218c251a77e8607706
suhayb <iamsuhaybahmed@gmail.com>
2025-01-20 02:04:02 +0530

    feat: add cors

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,13 @@
-import Fastify from "fastify";
 import dotenv from "dotenv";
+import Fastify from "fastify";
 import { hackathonRouter } from "./routes/hackathon.router.js";
 import { projectRouter } from "./routes/project.router.js";
+import cors from "@fastify/cors";
 
 dotenv.config();
 
 const fastify = Fastify({});
-
+await fastify.register(cors, {});
 fastify.setErrorHandler((error, request, reply) => {
   fastify.log.error(error);
   reply.status(error.statusCode ?? 500).send({
@@ -22,7 +23,7 @@ fastify.get("/", (request, reply) => {
 fastify.register(hackathonRouter, { prefix: "/api/hackathon" });
 fastify.register(projectRouter, { prefix: "/api/project" });
 
-fastify.listen({ port: 3000 }, (err, address) => {
+fastify.listen({ port: process.env.PORT }, (err, address) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
